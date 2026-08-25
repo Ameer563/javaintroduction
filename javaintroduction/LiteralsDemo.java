b2e767968ecabbcb81ec3ded95aa340d4d66a5f1
Ameer563 <ameergurajala27@gmail.com>
2026-08-25 11:58:11 +0530

    Add files via upload

---
+++ javaintroduction/LiteralsDemo.java
@@ -0,0 +1,28 @@
+package javaintroduction;
+
+public class LiteralsDemo {
+
+	public static void main(String[] args) {
+		int a=10;//Integer literals
+		float b=123f;//Floating point literals
+		char c='A';//character literal
+		String d="java";//string literal
+		boolean e=true;//boolean literal
+		double f=0x62d;//double literals
+		//int z; compile time error because we can't initialize the value of z in main method.
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+
+		
+
+
+
+
+	}
+
+}
