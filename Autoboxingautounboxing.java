9afcc7321a1b2756b332ae9104c608637e222019
Ameer563 <ameergurajala27@gmail.com>
2026-08-25 11:42:36 +0530

    Add files via upload

---
+++ Autoboxingautounboxing.java
@@ -0,0 +1,20 @@
+package languagefundamentals.src.languagefundamentals;
+
+public class Autoboxingautounboxing {
+	
+
+	public static void main(String[] args) {
+		//Auto boxing--> converting primitive data type to object data type.
+		int a=10;//integer primitive data type value
+		Integer i=a;//Integer object data type
+		System.out.println(i);
+		
+		//AutoUnboxing-->converting object data type to primitive data type.
+		Integer m=20;//object data type
+		int b=m; //converting to primitive data type.
+		System.out.println(b);
+		
+
+	}
+
+}
